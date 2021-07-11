a20d690082c2674def964563a6d9536f23f443a2
rysiek21 <rysiek21@users.noreply.github.com>
2021-07-11 22:09:29 +0200

    help update

--- cogs/help.py
+++ cogs/help.py
@@ -13,7 +13,7 @@ async def help(ctx):
   embed=discord.Embed(title="Help", color=0x1092c9)
   embed.add_field(name=f"{os.getenv('PREFIX')}Drop [surowiec]", value='Pokazuje drop wybranego surowca', inline=False)
   embed.add_field(name=f"{os.getenv('PREFIX')}Warframe [warframe]", value='Pokazuje drop wybranego Warframe', inline=False)
-  embed.add_field(name=f"{os.getenv('PREFIX')}Invasions", value='Pokazuje aktywne inwazje #WIP', inline=False)
+  embed.add_field(name=f"{os.getenv('PREFIX')}Invasions", value='Pokazuje aktywne inwazje', inline=False)
   embed.add_field(name=f"{os.getenv('PREFIX')}Sortie", value='Pokazuje aktywne sortie', inline=False)
   embed.add_field(name=f"{os.getenv('PREFIX')}Timers", value='Pokazuje cykl świata', inline=False)
   embed.set_footer(text=f'{useTime} • Requested by {ctx.author}')
