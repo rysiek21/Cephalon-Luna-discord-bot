22b2d5c75eda2a0c67c1409310a2c3b3edde5acc
rysiek21 <rysiek21@users.noreply.github.com>
2021-07-11 22:12:26 +0200

    Fix multi word resources

--- cogs/drop.py
+++ cogs/drop.py
@@ -8,10 +8,12 @@ with open('./drop-data.json', encoding='utf-8') as x:
   data = json.load(x)
 
 @commands.command()
-async def drop(ctx, arg):
+async def drop(ctx, *args):
+  arg = ''.join(args)
   arg = arg.lower()
   useTime = time.strftime("%H:%M", time.localtime())
-  embed=discord.Embed(title=arg.capitalize(), description="", color=0x1092c9)
+  for item in data[arg]:
+    embed=discord.Embed(title=item['label'], description="", color=0x1092c9)
   i = 0
   for item in data[arg]:
     embed.set_thumbnail(url=item['image'])
